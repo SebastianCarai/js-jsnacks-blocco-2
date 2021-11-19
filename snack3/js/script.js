f1a51f9aab2980f2e70ef5f73337e1bdc20f3667
SebastianCarai <sebastiancarai7@gmail.com>
2021-11-19 16:35:30 +0100

    snack3 done

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,7 +1,11 @@
 const numbers = [10,5,13,33,7,2,6,10];
 
-let somma=0;
-for (i=1; i<numbers.length; i+=2){
-    somma += numbers[i];
-}
-alert(somma);
+if(numbers.length>2){
+    let somma=0;
+    for (i=1; i<numbers.length; i+=2){
+        somma += numbers[i];
+    }
+    alert(somma);
+} else{
+    alert('non ci sono numeri in posizioni dispari')
+}    
